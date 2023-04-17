58e0ad8f32b41a88125f9a73e5a0bce14cf3d71d
hellodword <46193371+hellodword@users.noreply.github.com>
2023-04-17 04:38:13 +0000

    import mdict_utils instead of skywind3000/writemdict

--- README.md
+++ README.md
@@ -30,6 +30,7 @@
 - https://bitbucket.org/xwang/mdict-analysis
 - https://github.com/liuyug/mdict-utils
 - https://github.com/skywind3000/writemdict
+- https://github.com/liuyug/mdict-utils
 - https://github.com/skywind3000/ECDICT/blob/bc6b25957f1dbb745768bb734b20a613edf7fdbe/stardict.py#L1433
 - https://github.com/Dictionaryphile/All_Dictionaries
 - https://github.com/goldendict/goldendict/wiki/Supported-Dictionary-Formats
--- wiktionary2dict/app.py
+++ wiktionary2dict/app.py
@@ -98,13 +98,12 @@ class Wiktionary2Dict:
 
         parse_wiktionary('./data/en.sample.xml.bz2', wikitext_cb=wikitext_cb)
 
-        from .skywind3000.writemdict.writemdict import MDictWriter
-
         dictionary = {"doe": "a deer, a female deer.",
                       "ray": "a drop of golden sun.",
                       "me": "a name I call myself.",
                       "far": "a long, long way to run."}
 
+        from mdict_utils.base.writemdict import MDictWriter
         writer = MDictWriter(dictionary,
                              title="Example Dictionary",
                              description="This is an example dictionary.",
